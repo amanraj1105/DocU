321f633dea3ddc797aa4f3c2bb136a42670c66a8
amanraj1105 <aman1105raj@gmail.com>
2024-12-24 03:42:51 +0530

    topdoc

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors:{
         'primary':"#5fbfff"
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill,minmax(200px,1fr))'
       }
     },
   },
